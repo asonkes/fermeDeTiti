0474b93fbda8a6910c367765b55c7e46fe26a4f1
asonkes <audrey.sonkes@gmail.com>
2024-08-13 21:52:33 +0200

    dernier détail header en ordre

--- assets/JS/header/header.js
+++ assets/JS/header/header.js
@@ -6,42 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu__submenu');
     const header = document.querySelector('.header');
     const article = document.querySelector('.article');
-    const menuLink = document.querySelectorAll('.menu__link');
-
-    const headerCartImage = document.querySelector('.header__cart-image');
-    const headerAccountImage = document.querySelector('.header__account-image');
-
-    const headerCartImage2 = document.querySelector('.header__cart-image2');
-    const headerAccountImage2 = document.querySelector('.header__account-image2');
-
-    if(article) {
-        menuLink.forEach((e) => {
-            e.classList.add('active2');
-
-            headerCartImage.classList.add('active');
-            headerAccountImage.classList.add('active');
-    
-            headerCartImage2.classList.add('active');
-            headerAccountImage2.classList.add('active');
-
-            document.addEventListener('scroll', () => {
-                e.classList.remove('active2');
-                e.classList.add('active');
-
-                headerCartImage.classList.remove('active');
-                headerAccountImage.classList.remove('active');
-    
-                headerCartImage2.classList.remove('active');
-                headerAccountImage2.classList.remove('active');
-            })
-        }) 
-    }
 
     linkSpecial.addEventListener('click', (e) => {
         e.preventDefault();
         if(menu) {
             menu.classList.toggle('active');
-            header.classList.toggle('active');
+            
+            if(!article) {
+                header.classList.toggle('active');
+            }
+        
             linkIcon.classList.toggle('active');
         }
     })
@@ -62,29 +36,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if(scrollTop === 0) {
             header.classList.remove('active2');
-            header.classList.remove('active');
-            menu.classList.remove('active');
+            header.classList.add('active');
             menu.classList.remove('active2');
-
-            if(article) {
-                menuLink.forEach((e) => {
-                    e.classList.add('active2');
-                }) 
-
-                headerCartImage.classList.add('active');
-                headerAccountImage.classList.add('active');
-    
-                headerCartImage2.classList.add('active');
-                headerAccountImage2.classList.add('active');
-            }
         }
     })
+
+    // on load
+    if (article) {
+        header.classList.add('active');
+    }
     
     window.addEventListener('click', (event) => {
         // Si le menu est actif et que le clic ne vient ni du menu ni du lien, on ferme le menu
         if (menu.classList.contains('active') && !menu.contains(event.target) && !linkSpecial.contains(event.target)) {
+            if (!article) {
+                header.classList.remove('active');
+            }
             menu.classList.remove('active');
-            header.classList.remove('active');
             linkIcon.classList.remove('active');
         }
     });
