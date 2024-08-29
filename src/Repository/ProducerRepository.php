f71dd535a17414fb35538126213756f7aecc00ac
asonkes <audrey.sonkes@gmail.com>
2024-08-29 08:47:56 +0200

    css

--- src/Repository/ProducerRepository.php
+++ src/Repository/ProducerRepository.php
@@ -16,28 +16,28 @@ class ProducerRepository extends ServiceEntityRepository
         parent::__construct($registry, Producer::class);
     }
 
-//    /**
-//     * @return Producer[] Returns an array of Producer objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Producer[] Returns an array of Producer objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('p.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Producer
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Producer
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
--- src/Service/JWTService.php
+++ src/Service/JWTService.php
@@ -3,7 +3,6 @@
 namespace App\Service;
 
 use DateTimeImmutable;
-use Doctrine\DBAL\Types\DateImmutableType;
 
 class JWTService
 {
