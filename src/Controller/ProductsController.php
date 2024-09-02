7cf5dab7afe0afd6e89db881b45f8e8dfc4d6eda
asonkes <audrey.sonkes@gmail.com>
2024-09-02 23:29:57 +0200

    pagination ok - css pagination en ordre

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductsRepository;
 use App\Repository\CategoriesRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,8 +13,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProductsController extends AbstractController
 {
     #[Route('/{slug}', name: 'index')]
-    public function index(string $slug, CategoriesRepository $categoriesRepository): Response
+    public function index(string $slug, CategoriesRepository $categoriesRepository, ProductsRepository $productsRepository, Request $request): Response
     {
+        // On va chercher le numéro de page dans l'url
+        $page = $request->query->getInt('page', 1);
+
         // Ici on a récupéré le nom de la catégorie spécifique
         $category = $categoriesRepository->findOneBy(['slug' => $slug]);
 
@@ -20,7 +25,9 @@ class ProductsController extends AbstractController
             throw $this->createNotFoundException('Catégorie non trouvée');
         }
 
-        $products = $category->getProducts();
+        // Pagination commence
+        // (1, $category->getSlug(), 2) ==> 1 est le numéro de la page et 2 la limite
+        $products = $productsRepository->findProductsPaginated($page, $category->getSlug(), 6);
 
         return $this->render('products/index.html.twig', [
             'category' => $category,
--- src/Repository/ProductsRepository.php
+++ src/Repository/ProductsRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Products;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Products>
@@ -16,28 +17,44 @@ class ProductsRepository extends ServiceEntityRepository
         parent::__construct($registry, Products::class);
     }
 
-//    /**
-//     * @return Products[] Returns an array of Products objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    // Permet de débuter la pagination
+    public function findProductsPaginated(int $page, string $slug, int $limit = 6): array
+    {
+        //pagination commence
+        // abs (valeur absolue) ==> permet de toujours avoir une limite même si on met une valeur négative
+        $limit = abs($limit);
 
-//    public function findOneBySomeField($value): ?Products
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        $result = [];
+
+        $query = $this->getEntityManager()->createQueryBuilder()
+            // c= catégories et 'p' = products
+            ->select('c', 'p')
+            // On va cherches les products
+            ->from('App\Entity\Products', 'p')
+            // On va chercher la catégorie correspondante aux produits (p.categories)
+            ->join('p.categories', 'c')
+            ->where("c.slug = '$slug'")
+            ->setMaxResults($limit)
+            // Permet d'aller chercher juste les produits qui correspondent à ma page et à ma limit
+            ->setFirstResult(($page * $limit) - $limit);
+
+        $paginator = new Paginator($query);
+        $data = $paginator->getQuery()->getResult();
+
+        // On vérifie que l'on a des données
+        if (empty($data)) {
+            return $result;
+        }
+
+        // On calcule le nombre de pages
+        $pages = ceil($paginator->count() / $limit);
+
+        // On remplit le tableau
+        $result['data'] = $data;
+        $result['pages'] = $pages;
+        $result['page'] = $page;
+        $result['limit'] = $limit;
+
+        return $result;
+    }
 }
